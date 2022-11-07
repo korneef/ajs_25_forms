6bfad1c13388a7eb1a7e6b1bfef7c5a7f38a71fb
Valentin Korneev <v.korneef@gmail.com>
2022-11-08 01:16:08 +0600

    e2e added

---
+++ e2e/test.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import puppeteer from 'puppeteer';
+
+jest.setTimeout(90000);
+
+describe('e2e start test', () => {
+  let browser = null;
+  let page = null;
+  const baseUrl = 'http://localhost:8888';
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 100,
+      devtools: true,
+    });
+    page = await browser.newPage();
+  });
+
+  test('button test', async () => {
+    await page.goto(baseUrl);
+    const button = await page.$('.button-toggle__button');
+    button.click();
+    await page.waitForSelector('.button-toggle__popower-form');
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
